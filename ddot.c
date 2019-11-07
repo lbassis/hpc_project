6687a95d19a2bd479c7338c6737551d0e075787f
Lucas Barros de Assis <lucas.barros_de_assis@enseirb-matmeca.fr>
2019-11-07 15:14:59 +0100

    dgemm_scalaire ok

--- ddot.c
+++ ddot.c
@@ -1,3 +1,4 @@
+#include <stdlib.h>
 #include "ddot.h"
 
 double my_ddot(const int N, const double *X, const int incX, const double *Y, const int incY) {
@@ -9,4 +10,20 @@ double my_ddot(const int N, const double *X, const int incX, const double *Y, co
   }
 
   return result;
+}
+
+double *my_dgemm_scalaire(int m, double *a, double *b) {
+ 
+  int i, j, k;
+  double *c = calloc(sizeof(double), m*m);
+  
+  for (i = 0; i < m; i++) { //
+    for (j = 0; j < m; j++) { // 
+      for (k = 0; k < m; k++) { // 
+	c[i+m*j] += a[k+m*i]*b[k+m*j];
+      }
+    }
+  }
+
+  return c;
 }
