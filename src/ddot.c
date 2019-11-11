de7188914f3567656df3ec4b686ab7dd8efc46ed
Lucas Barros de Assis <lbassis@inf.ufrgs.br>
2019-11-11 19:52:40 +0100

    trsm implemented based on lapack's code

--- src/ddot.c
+++ src/ddot.c
@@ -99,15 +99,15 @@ void my_dgemm(int transA, int transB, int m, int n, int k, double alpha, double 
 	  tmp += a[j+lda*i]*b[j*ldb+l];
 	}
       }
-      c[i+m*l] = c[i+m*l]*beta + tmp*alpha;
+      c[i+ldc*l] = c[i+ldc*l]*beta + tmp*alpha;
     } 
   }
 }
 
-void my_daxpy (int n, double a, double *x, int incx, double *y, int incy) {
+void my_daxpy (int n, double a, double *x, int incX, double *y, int incY) {
   int i;
   for (i = 0; i < n; i++) {
-    y[i] += a*x[i];
+    y[i*incY] += a*x[i*incX];
   }
   
 }
@@ -151,10 +151,9 @@ void my_dgetf2( int m, int n, double* a, int lda, int* ipiv ) {
 
   int i, j, k, x;
   double *u = malloc(sizeof(double)*n);
-  double iik;
 
   for (k = 0; k < m; k++) {
-    for (x = 0; x < n; x++) { // copia toda a linha k pra u
+    for (x = 0; x < n; x++) { 
       u[x] = a[lda*x+k];
     }
     for (i = k+1; i < m; i++) {
@@ -164,4 +163,184 @@ void my_dgetf2( int m, int n, double* a, int lda, int* ipiv ) {
       }
     }
   }
+
+  (void)ipiv;
 }
+
+void my_dtrsm (int *Layout, int side, int uplo, int transA, int diag, int m, int n, double alpha, double *a, int lda, double *b, int ldb) {
+
+  int i, j, k;
+  double temp, one = 1.;
+  
+  if (side == 0) { // left
+    if (!transA) { // !transA
+      if (uplo == 0) { // no trans, left, upper
+	for (j = 0; j < n; j++) {
+	  if (alpha != 1.) {
+	    for (i = 0; i < m; i++) {
+	      b[i+ldb*j] = alpha*b[i+ldb*j];
+	    }
+	  }
+	  for (k = m; k > 0; k--) {
+	    if (b[k+ldb*j] != 0) {
+	      if (diag == 0) {
+		b[k+ldb*j] /= a[k+lda*k];
+	      }
+	      for (i = 0; i < k-1; i++) {
+		b[i+ldb*j] -= b[k+ldb*j]*a[i+lda*k];
+	      }
+	    }
+	  }
+	}
+      }
+      else { // no trans, left, lower
+	for (j = 0; j < n; j++) {
+	  if (alpha != 1.) {
+	    for (i = 0; i < m; i++) {
+	      b[i+ldb*j] = alpha*b[i+ldb*j];
+	    }
+	  }
+	  for (k = 0; k < m; k++) {
+	    if (b[k+ldb*j] != 0) {
+	      if (diag == 0) {
+		b[k+ldb*j] /= a[k+lda*k];
+	      }
+	      for (i = 0; i < k-1; i++) {
+		b[i+ldb*j] -= b[k+ldb*j]*a[i+lda*k];
+	      }
+	    }
+	  }
+	}
+      }
+    }
+    else { //transA
+      if (uplo == 0) { // trans, left, upper
+	for (j = 0; j < n; j++) {
+	  for (i = 0; i < m; i++) {
+	    temp = alpha*b[i+ldb*j];
+	    for (k = 0; k < i-1; k++) {
+	      temp -= a[k+lda*i]*b[k+ldb*j];
+	    }
+	    if (diag == 0) {
+		temp /= a[i+lda*i];
+	    }
+	    b[i+ldb*j] = temp;
+	  }
+	}
+      }
+      else { // trans, left, lower
+	for (j = 0; j < n; j++) {
+	  for (i = m; i > 0; i--) {
+	    temp = alpha*b[i+ldb*j];
+	    for (k = i+1; k < m; k++) {
+	      temp -= a[k+lda*i]*b[k+ldb*j];
+	    }
+	    if (diag == 0) {
+		temp /= a[i+lda*i];
+	    }
+	    b[i+ldb*j] = temp;
+	  }
+	}
+      }
+    }
+  }
+  else { // side = right
+    if (!transA) {
+      if (uplo == 0) { // no trans, right, upper
+	for (j = 0; j < n; j++) {
+	  if (alpha != 1.) {
+	    for (i = 0; i < m; i++) {
+	      b[i+ldb*j] = alpha*b[i+ldb*j];
+	    }
+	  }
+	  for (k = 1; k < j-1; k++) {
+	    if (a[k+ldb*j] != 0) {
+	      for (i = 0; i < m; i++) {
+		b[i+ldb*j] -= a[k+lda*j]*b[i+ldb*k];
+	      }
+	    }
+	  }
+	  if (diag == 0) {
+	    temp = one/a[j+lda*j];
+	    for (i = 0; i < m; i++) {
+	      b[i+ldb*j] *= temp;
+	    }
+	  }
+	}
+      }
+      else { // no trans, right, lower
+	for (j = 0; j < n; j++) {
+	  if (alpha != 1.) {
+	    for (i = 0; i < m; i++) {
+	      b[i+ldb*j] = alpha*b[i+ldb*j];
+	    }
+	  }
+	  for (k = j+1; k < n; k++) {
+	    if (a[k+ldb*j] != 0) {
+	      for (i = 0; i < m; i++) {
+		b[i+ldb*j] -= a[k+lda*j]*b[i+ldb*k];
+	      }
+	    }
+	  }
+	  if (diag == 0) {
+	    temp = one/a[j+lda*j];
+	    for (i = 0; i < m; i++) {
+	      b[i+ldb*j] *= temp;
+	    }
+	  }
+	}
+      }
+    }
+    else { //transA      
+      if (uplo == 0) { // trans, right, upper
+	for (k = n; k >0; k--) {
+	  if (diag == 0) {
+	    temp = one/a[k+lda*k];
+	    for (i = 0; i < m; i++) {
+	      b[i+ldb*k] *= temp;
+	    }
+	  }
+	  for (j = 0; j < k-1; j++) {
+	    if (a[j+lda*k] != 0) {
+	      temp = a[j+lda*k];
+	      for (i = 0; i < m; i++) {
+		b[i+ldb*j] -= b[i+ldb*k];
+	      }
+	    }
+	  }
+	  if (alpha != 1) {
+	    for (i = 0; i < m; i++) {
+	      b[i+ldb*k] *= alpha;
+	    }
+	  }
+	}
+      }
+      else { // trans, right, lower
+	for (k = 0; k < n; k++) {
+	  if (diag == 0) {
+	    temp = one/a[k+lda*k];
+	    for (i = 0; i < m; i++) {
+	      b[i+ldb*k] *= temp;
+	    }
+	  }
+	  for (j = k+1; j < n; j++) {
+	    if (a[j+lda*k] != 0) {
+	      temp = a[j+lda*k];
+	      for (i = 0; i < m; i++) {
+		b[i+ldb*j] -= b[i+ldb*k];
+	      }
+	    }
+	  }
+	  if (alpha != 1) {
+	    for (i = 0; i < m; i++) {
+	      b[i+ldb*k] *= alpha;
+	    }
+	  }
+	}
+      }
+    }
+  }
+
+  (void)Layout;
+}
+      
