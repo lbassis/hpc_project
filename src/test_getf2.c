8ee8d44195784205cc589c0f922f0ecd724dd33c
Lucas Barros-De-Assis <lbassis@inf.ufrgs.br>
2019-11-13 19:10:20 +0100

    trying to find the problems

--- src/test_getf2.c
+++ src/test_getf2.c
@@ -2,35 +2,33 @@
 #include <stdlib.h>
 #include <mkl_cblas.h>
 #include <mkl.h>
-
 #include "util.h"
 #include "ddot.h"
 #include "perf.h"
 
-#define INIT_VEC(N, ...) {int i = 0;              \
-                          for(i = 0; i < N; i++)  \
-                            __VA_ARGS__;          \
-                         }
 #ifndef SIZE
-#define SIZE 5
+#define SIZE 20
 #endif
 
 int main(void){
-  double a[SIZE * SIZE] = {};
-  double b[SIZE * SIZE] = {};
-  INIT_VEC(SIZE * SIZE, a[i] = b[i] = i+1;)
 
-  affiche(SIZE, SIZE, a, SIZE, stdout);
-  printf("\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n");
-  affiche(SIZE, SIZE, b, SIZE, stdout);
-  printf("\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n");
+  double *a, *b;
+  int i;
+  long long ipiv[SIZE] = {};
 
+  a = alloc_mat(SIZE, SIZE);
+  b = alloc_mat(SIZE, SIZE);
+  init_random(SIZE, SIZE, &a, 1);
+  for (i = 0; i < SIZE*SIZE; i++) {
+    b[i] = a[i];
+  }
 
   my_dgetf2(SIZE, SIZE, a, SIZE, NULL);
-  affiche(SIZE, SIZE, a, SIZE, stdout);
-  printf("\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n");
-  long long ipiv[SIZE] = {};
   LAPACKE_dgetf2(LAPACK_COL_MAJOR, SIZE, SIZE, b, SIZE, ipiv);
+
+  for (i = 0; i < SIZE*SIZE; i++) {
+    b[i] -= a[i];
+  }
 
   affiche(SIZE, SIZE, b, SIZE, stdout);
   return 0;
