cc468198d9927d4136b7b38bea9bba918efcf921
Lucas Barros-De-Assis <lbassis@inf.ufrgs.br>
2019-11-16 18:14:02 +0100

    finishing merge

--- src/my_dgemm.c
+++ src/my_dgemm.c
@@ -117,22 +117,44 @@ void my_dgemm_seq(CBLAS_LAYOUT layout,
 
 void my_dgemm(int transA, int transB, int m, int n, int k, double alpha, double *a, int lda, double *b, int ldb, double beta, double *c, int ldc) {
 
-  int i, j, l, q, r;
+  int i, j, kk;
 
-  int nb_bloc_n = n / BLOC_SIZE;
-  int nb_bloc_m = m / BLOC_SIZE;
-  int a_bloc_start, b_bloc_start, c_bloc_start;
+  int nb_bloc_n = (n + BLOC_SIZE - 1) / BLOC_SIZE;
+  int nb_bloc_m = (m + BLOC_SIZE - 1) / BLOC_SIZE;
+  int nb_bloc_k = (k + BLOC_SIZE - 1) / BLOC_SIZE;
 
-  for (r = 0; r < nb_bloc_n; r++) {
-    for (q = 0; q < nb_bloc_m; q++) {
+  int current_block_n;
+  int current_block_m;
+  int current_block_k;
 
-       c_bloc_start = q*BLOC_SIZE + (r*BLOC_SIZE*nb_bloc_m);
+  int start_a, start_b, start_c;
+  double current_beta = beta;
 
-       for (i = 0; i < nb_bloc_n; i++) {
-	        a_bloc_start = q*BLOC_SIZE + (i*BLOC_SIZE*nb_bloc_m);
-	        b_bloc_start = i*BLOC_SIZE + (r*BLOC_SIZE*nb_bloc_m);
-	        //my_dgemm_backup(transA, transB, BLOC_SIZE, BLOC_SIZE, BLOC_SIZE, 1., a+a_bloc_start, BLOC_SIZE, b+b_bloc_start, BLOC_SIZE, 1., c+c_bloc_start, BLOC_SIZE);
+  printf("nb blocks: %d, %d e %d\n", nb_bloc_m, nb_bloc_n, nb_bloc_k);
+  for (j = 0; j < nb_bloc_n; j++) { // colonnes de B
+    for (kk = 0; kk < nb_bloc_k; kk++) { // colonnes de A
+      start_c = (kk + ldc*j)*BLOC_SIZE;
+
+      current_block_n = (j < nb_bloc_n - 1) ? BLOC_SIZE : n - j*BLOC_SIZE;
+      current_block_k = (kk < nb_bloc_k - 1) ? BLOC_SIZE : k - kk*BLOC_SIZE;
+      printf("C[%d] = ", start_c);
+
+      for (i = 0; i < nb_bloc_m; i++) { // lignes de A
+	current_block_m = (i < nb_bloc_m - 1) ? BLOC_SIZE : m - i*BLOC_SIZE;
+	start_a = (kk + lda*i)*BLOC_SIZE;
+	printf("[%d + %d*%d]\n", j, ldb, kk);
+	start_b = (kk + ldb*j)*BLOC_SIZE;
+	printf("+A[%d]*B[%d](%d, %d, %d) ", start_a, start_b, current_block_m, current_block_n, current_block_k);
+	cblas_dgemm(CblasColMajor, CblasNoTrans, CblasNoTrans,
+		    current_block_m, current_block_n,
+		    current_block_k, alpha,
+		     a+start_a, lda,
+		     b+start_b, ldb, current_beta,
+		     c+start_c, ldc);
+	current_beta = 1;
       }
+      printf("\n");
+      current_beta = beta;
     }
   }
 }
--- src/test_perf_my_dgemm_scalaire.c
+++ src/test_perf_my_dgemm_scalaire.c
@@ -1,8 +1,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-#include "util.h"
-#include "perf.h"
+#include "my_lib.h"
 #include <mkl.h>
 
 #define INIT_VEC(N, ...) {int i = 0;              \
@@ -38,14 +37,12 @@ void test_version(char *id, void (*gemm)(int, int, int, int, int, int, double, d
   int IONE = 1;
   long long int   ISEED[4] = {0,0,0,1};   /* initial seed for zlarnv() */
   long nb_loop = NB_LOOP;
-  long flop = 3*n*n;
 
   double performance;
   perf_t start,stop;
 
   // Performance d'une addition scalaire
   int l = 0, n = 0;
-<<<<<<< HEAD
   double *a = alloc_mat((unsigned long)MAX_SIZE, (unsigned long)MAX_SIZE);
   double *b = alloc_mat((unsigned long)MAX_SIZE, (unsigned long)MAX_SIZE);
   double *c = alloc_mat((unsigned long)MAX_SIZE, (unsigned long)MAX_SIZE);
@@ -56,32 +53,10 @@ void test_version(char *id, void (*gemm)(int, int, int, int, int, int, double, d
 
   double result = 0;
   for(n = MIN_SIZE; n < MAX_SIZE; n += n / 4){
+    long flop = 3*n*n;
     perf(&start);
     for(l = 0; l < nb_loop; l++){
       (*gemm)(CblasColMajor, CblasNoTrans, CblasNoTrans, n, n, n, 1., a, n, b, n, 0, c, n);
-=======
-  double *a = alloc_vec(MAX_SIZE * MAX_SIZE), *b = alloc_vec(MAX_SIZE * MAX_SIZE), *c = alloc_vec(MAX_SIZE * MAX_SIZE);
-  INIT_VEC(MAX_SIZE * MAX_SIZE, a[i] = i + 1; b[i] = i + 1)//larnv
-  long nb[2] = {MIN_SIZE, MAX_SIZE};
-  for(n = 0;  n < 1 ; n++){
-    int func = 0;
-    printf("%ld, ", nb[n]);
-    for(func = 0; func < 4; func++){
-      perf(&start);
-      for(l = 0; l < nb_loop; l++){
-        (*((void (*)(int, double*, double*, double*))tab[func]))(nb[n], a, b, c);
-      }
-      perf(&stop);
-      perf_diff(&start, &stop);
-      performance = perf_mflops(&stop, flop * nb[n]);
-      printf("%lf, ", performance);
-      perf_print_time(&stop, nb_loop);
-      printf(",");
-      // Verification
-      /*if(fabs(result - cblas_ddot(n, a, 1, b, 1)) > ESP){
-        printf("FAIL\n");
-      }*/
->>>>>>> 1c60e3f13fc745edaf6ed65a951ed5ec34130cd9
     }
     perf(&stop);
     perf_diff(&start, &stop);
