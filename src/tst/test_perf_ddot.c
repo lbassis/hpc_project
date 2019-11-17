6ee513a2eae41edb367d5846aa6cacb9e8a34f3b
Lucas Barros-De-Assis <lbassis@inf.ufrgs.br>
2019-11-17 12:02:11 +0100

    test performance ddot

---
+++ src/tst/test_perf_ddot.c
@@ -0,0 +1,82 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <mkl.h>
+#include "util.h"
+#include "perf.h"
+#include "my_lib.h"
+
+#ifndef NB_LOOP
+#define NB_LOOP 100000000
+#endif
+
+#ifndef MIN_SIZE
+#define MIN_SIZE 256
+#endif
+
+#ifndef MAX_SIZE
+#define MAX_SIZE 1048576
+#endif
+
+
+int test_version(char *id, int step, double (*ddot)(const int, const double*, const int, const double*, const int)) {
+
+  long long int   ISEED[4] = {0,0,0,1};   /* initial seed for zlarnv() */
+  
+  int l, nb_loop = NB_LOOP;
+  double performance;
+  perf_t start,stop;
+  // Executions a vide, flush potentiel, ...
+
+  // Performance d'une addition scalaire
+  int n = 0;
+  double *a = alloc_vec(MAX_SIZE);
+  double *b = alloc_vec(MAX_SIZE);
+  
+  LAPACKE_dlarnv_work(1, ISEED, MAX_SIZE, a);
+  LAPACKE_dlarnv_work(1, ISEED, MAX_SIZE, b);
+
+  double result;
+  for(n = MIN_SIZE; n < MAX_SIZE; n *= 2){
+    long flop = 2*n/step;
+    
+    perf(&start);
+    for(l = 0; l < nb_loop; l++){
+      result = ddot(n/step, a, step, b, step);
+    }
+    perf(&stop);
+    perf_diff(&start, &stop);
+    performance = perf_mflops(&stop, flop * nb_loop);
+    printf("%s, %d, %d, %lf, ", id, n, step, performance);
+
+    perf_print_time(&stop, nb_loop);
+    printf("\n");
+
+    nb_loop = (int)nb_loop/1.5;
+  }
+
+  free(a);
+  free(b);
+
+  return result;
+}
+
+
+int main() {
+  printf("version, n, step, Mflops, us\n");
+
+  double my_one = test_version("my", 1, &my_ddot);
+  double mkl_one = test_version("mkl", 1, &cblas_ddot); 
+  double my_two = test_version("my", 2, &my_ddot);
+  double mkl_two = test_version("mkl", 2, &cblas_ddot); 
+  
+             
+
+  if (my_one == mkl_one && my_two == mkl_two) {
+    printf("\nOK\n");
+  }
+  else {
+    printf("\nFailed\n");
+  }
+    
+  return 0;
+}
