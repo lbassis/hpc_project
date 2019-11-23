abf2e7038f57da78dc642846d53ca0276e86aaa4
Lucas Barros-De-Assis <lbassis@inf.ufrgs.br>
2019-11-23 17:36:24 +0100

    dgemm tiled implemented

--- headers/util.h
+++ headers/util.h
@@ -2,3 +2,11 @@ void affiche(unsigned long m, unsigned long n, double *a, unsigned long lda, FIL
 double *alloc_mat(unsigned long m, unsigned long n);
 double *alloc_vec(unsigned long n);
 int init_random(unsigned long m, unsigned long n, double *a, unsigned int seed);
+
+double **
+lapack2tile( int M, int N, int b,
+             const double *Alapack, int lda );
+void
+tile2lapack( int M, int N, int b,
+             const double **Atile,
+             double *A, int lda );
