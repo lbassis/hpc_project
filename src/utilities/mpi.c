ed1df2dc93aee72570c72d6ffd176627eb500393
Lucas Barros-De-Assis <lbassis@inf.ufrgs.br>
2019-12-07 15:26:43 +0100

    scatter certainly works but need to reverse p and q

--- src/utilities/mpi.c
+++ src/utilities/mpi.c
@@ -39,14 +39,17 @@ void scatter_matrix(const int m,
   MPI_Status status;
 
   if(me == 0){
+    printf("scattering\n");
     for( i = 0; i < nb_bloc_m; i++) {
       for( j = 0; j < nb_bloc_n; j++) {
-        int proc = (i%nb_bloc_m) + nb_bloc_m*(j%nb_bloc_n);
+	int proc = j%dim[1]+(i*dim[1])%(dim[0]*dim[1]);
         if(proc == 0){
           out[i_out + j_out * m_out] = (double*) malloc(TILE_SIZE * TILE_SIZE * sizeof(double));
-          LAPACKE_dlacpy_work( LAPACK_COL_MAJOR, 'A', TILE_SIZE, TILE_SIZE,
+          LAPACKE_dlacpy( LAPACK_COL_MAJOR, 'A', TILE_SIZE, TILE_SIZE,
                                in[i + j * nb_bloc_m], TILE_SIZE,
                                out[i_out + j_out * m_out], TILE_SIZE);
+	  /* printf("p0:\n"); */
+	  /* affiche(TILE_SIZE, TILE_SIZE, out[i_out+j_out*m_out], TILE_SIZE, stdout); */
           if(j_out == n_out - 1){
             j_out = 0;
             i_out++;
@@ -67,13 +70,13 @@ void scatter_matrix(const int m,
       for(j_out = 0; j_out < n_out; j_out++){
         out[i_out + j_out * m_out] = (double*) malloc(TILE_SIZE * TILE_SIZE * sizeof(double));
         MPI_Recv(out[i_out + j_out * m_out], TILE_SIZE * TILE_SIZE, MPI_DOUBLE, 0, START, comm, &status);
+	sleep(me+1);
+	printf("p%d:\n", me);
+	affiche(TILE_SIZE, TILE_SIZE, out[i_out+j_out*m_out], TILE_SIZE, stdout);
       }
     }
-
   }
 }
-
-
 
 void gather_matrix(const int m,
                     const int n,
@@ -87,52 +90,38 @@ void gather_matrix(const int m,
   int nb_bloc_n = (n + TILE_SIZE - 1) / TILE_SIZE;
   int n_out = nb_bloc_n / dim[1] + (nb_bloc_n % dim[1] > me);
   int m_out = nb_bloc_m / dim[0] + (nb_bloc_n % dim[0] > me);
-  int i_out = 0, j_out = 0;
+  int i_local = 0, j_local = 0;
   int i,j;
   MPI_Status status;
   if(me == 0){
+    printf("gathering\n");
     for( i = 0; i < nb_bloc_m; i++) {
       for( j = 0; j < nb_bloc_n; j++) {
-        int proc = (i%nb_bloc_m) + nb_bloc_m * (j%nb_bloc_n);
-        if(proc == 0){
-          //printf("%d -> 0\n", proc);
-
-          //out[i_out + j_out * m_out] = (double*) malloc(TILE_SIZE * TILE_SIZE * sizeof(double));
-          LAPACKE_dlacpy_work( LAPACK_COL_MAJOR, 'A', TILE_SIZE, TILE_SIZE,
-                               in[i_out + j_out * nb_bloc_m], TILE_SIZE,
-                               out[i + j * m_out], TILE_SIZE);
-          //printf("%d -> 0\n", proc);
-
-          if(j_out == n_out - 1){
-            j_out = 0;
-            i_out++;
-            if(i_out == m_out - 1){
-              i_out = 0;
-            }
-          }else{
-            j_out++;
+	int proc = j%dim[1]+(i*dim[1])%(dim[0]*dim[1]);
+       if(proc == 0){
+          LAPACKE_dlacpy( LAPACK_COL_MAJOR, 'A', TILE_SIZE, TILE_SIZE,
+			       in[i_local + j_local * m_out], TILE_SIZE,
+			       out[i + j * nb_bloc_m], TILE_SIZE);
+          if(j_local == n_out - 1){
+            j_local = 0;
+            i_local ++;
+	    if (i_local == m_out) {
+	      i_local = 0;
+	    }
+	  }
+	  else{
+            j_local++;
           }
         }else{
-          printf("%d -> 0\n", proc);
           MPI_Recv(out[i + j * nb_bloc_m], TILE_SIZE * TILE_SIZE, MPI_DOUBLE, proc, proc, comm, &status);
         }
       }
     }
-    for(i_out = 0; i_out < nb_bloc_m; i_out++){
-      for(j_out = 0; j_out < nb_bloc_m; j_out++){
-        printf("______\n");
-        affiche(TILE_SIZE, TILE_SIZE, out[i_out + j_out * nb_bloc_m], TILE_SIZE, stdout);
-      }
-    }
-    printf("ok\n");
   }else{
 
-    for(i_out = 0; i_out < m_out; i_out++){
-      for(j_out = 0; j_out < n_out; j_out++){
-        sleep(me+1);
-        //out[i_out + j_out * m_out] = (double*) malloc(TILE_SIZE * TILE_SIZE * sizeof(double));
-        printf("%d ==> 0   (%d, %d)\n", me, m_out, n_out);
-        MPI_Send(in[i_out + j_out * m_out], TILE_SIZE * TILE_SIZE, MPI_DOUBLE, 0, me, comm);
+    for(i_local = 0; i_local < m_out; i_local++){
+      for(j_local = 0; j_local < n_out; j_local++){
+        MPI_Send(in[i_local + j_local * m_out], TILE_SIZE * TILE_SIZE, MPI_DOUBLE, 0, me, comm);
       }
     }
   }
